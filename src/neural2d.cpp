22880c10d434a461bc455ad3ce7f367e4fe76406
thayne <kikansenkyoshi@hotmail.com>
2016-10-13 23:12:49 -0600

    clean out gen

--- src/neural2d.cpp
+++ src/neural2d.cpp
@@ -7,57 +7,114 @@ For more info, see neural2d.h.
 
 #include "neural2d.h"
 
+enum MODE { TRAINING, VALIDATE, TRAINED };
+
+void printUsage(){
+    std::cerr << "Usage is [--mode {TRAINING|VALIDATION|TRAINED}] [--weights <weightsfile>] [--p] <topologyfile> <inputdatafile>\n";
+}
+
 int main(int argc, char **argv)
 {
     // We need two or three filenames -- we can define them here, or get them from
     // the command line. If they are specified on the command line, they must be in
     // the order: topology, input-data, and optionally, weights.
 
-    std::string topologyFilename = "topology.txt";   // Always needed
-    std::string inputDataFilename = "inputData.txt"; // Always needed
+    std::string topologyFilename = "";   // Always needed
+    std::string inputDataFilename = ""; // Always needed
     std::string weightsFilename = "weights.txt";     // Needed only if saving or restoring weights
+    MODE mode = TRAINING;
+    bool paused(false);
 
-    if (argc > 1) topologyFilename  = argv[1];
-    if (argc > 2) inputDataFilename = argv[2];
-    if (argc > 3) weightsFilename   = argv[3];
-
+    if (argc < 3) { // Check the value of argc. If not enough parameters have been passed, inform user and exit.
+        printUsage();
+        return 0;
+    } else { // if we got enough parameters...
+        char* myFile, myPath, myOutPath;
+        std::cout << argv[0];
+        for (int i = 1; i < argc; i++) {
+            if (i + 1 != argc){ // Check that we haven't finished parsing already
+                if (std::string(argv[i]) == "--mode") {
+                    std::string modestr = argv[++i];
+                    if (modestr == "VALIDATE"){
+                        mode = MODE::VALIDATE;
+                    } else if (modestr == "TRAINED"){
+                        mode = MODE::TRAINED;
+                    }//else leave as default ... TRAINING
+                } else if (std::string(argv[i]) == "--help") {
+                    printUsage();
+                } else if (std::string(argv[i]) == "--p") {
+                    paused = true;
+                    std::cout << "Paused." << std::endl;
+                } else if (std::string(argv[i]) == "--weights") {
+                    weightsFilename = argv[++i];
+                } else {
+                    if (topologyFilename == ""){
+                        topologyFilename = argv[i];
+                    } else if (inputDataFilename == ""){
+                        inputDataFilename = argv[i];
+                    } else {
+                        std::cerr << "Invalid parameter: " << argv[i] << "\n";
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
 
     NNet::Net myNet(topologyFilename);   // Create net, neurons, and connections
     myNet.sampleSet.loadSamples(inputDataFilename);
-
-    if (argc > 4 && argv[4][0] == '-' && argv[4][1] == 'p') {
+    if(paused){
         myNet.isRunning = false;
-        std::cout << "Paused." << std::endl;
     }
 
-    // Here is an example of TRAINING mode -------------:
-    // See the GitHub wiki for example code for VALIDATE and TRAINED modes:
-    // https://github.com/davidrmiller/neural2d/wiki
+    switch(mode){
+        case TRAINED:
+        case VALIDATE:
+            // Here is an example of VALIDATE mode -------------:
 
-    myNet.eta = 0.1f;
-    myNet.dynamicEtaAdjust = true;
-    myNet.alpha = 0.0f;
-    myNet.reportEveryNth = 1;
-    myNet.repeatInputSamples = true;
-    myNet.shuffleInputSamples = true;
-    myNet.doneErrorThreshold = 0.01f;
+            myNet.reportEveryNth = 1;
+            myNet.repeatInputSamples = false;
 
-    do {
-        if (myNet.shuffleInputSamples) {
-            myNet.sampleSet.shuffle();
-        }
+            myNet.loadWeights(weightsFilename); // Use weights from a trained net
 
-        for (auto &sample : myNet.sampleSet.samples) {
-            myNet.feedForward(sample);
-            myNet.backProp(sample);
-            myNet.reportResults(sample);
-            if (myNet.recentAverageError < myNet.doneErrorThreshold) {
-                std::cout << "Solved!   -- Saving weights..." << std::endl;
-                myNet.saveWeights(weightsFilename);
-                exit(0);
-            }
-        }
-    } while (myNet.repeatInputSamples);
+            do {
+                for (auto &sample : myNet.sampleSet.samples) {
+                    myNet.feedForward(sample);
+                    myNet.reportResults(sample);
+                }
+            } while (myNet.repeatInputSamples);
+            break;
+
+        case TRAINING:
+        default:
+            // Here is an example of TRAINING mode -------------:
+
+            myNet.eta = 0.1f;
+            myNet.dynamicEtaAdjust = true;
+            myNet.alpha = 0.0f;
+            myNet.reportEveryNth = 1;
+            myNet.repeatInputSamples = true;
+            myNet.shuffleInputSamples = true;
+            myNet.doneErrorThreshold = 0.01f;
+
+            do {
+                if (myNet.shuffleInputSamples) {
+                    myNet.sampleSet.shuffle();
+                }
+
+                for (auto &sample : myNet.sampleSet.samples) {
+                    myNet.feedForward(sample);
+                    myNet.backProp(sample);
+                    myNet.reportResults(sample);
+                    if (myNet.recentAverageError < myNet.doneErrorThreshold) {
+                        std::cout << "Solved!   -- Saving weights..." << std::endl;
+                        myNet.saveWeights(weightsFilename);
+                        exit(0);
+                    }
+                }
+            } while (myNet.repeatInputSamples);
+            break;
+    }
 
     std::cout << "Done." << std::endl;
 
